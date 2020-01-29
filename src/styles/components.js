adc4cf00fb906e6eb6a059c38e5e14b629c1dc13
Weimar Andres Castañeda Molina <wcastaneda@psl.com.co>
2020-01-29 16:02:34 -0500

    files load

---
+++ src/styles/components.js
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+
+export const colors = {
+  ligthBlue: "##21d4fd",
+  purple: "#B720FF",
+  darkPurple: "#2d2045",
+  textGray: "#888888",
+  red: "#e60023",
+  ligthGray: "#f5f5f5",
+  black: "#060606",
+}
+
+export const StyledHeader = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  padding: 10px 0 10px 42px;
+  z-index: 10000;
+  transition: all 0.5s ease;
+  height: auto !important;
+`
